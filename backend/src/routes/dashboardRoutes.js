6bc2223bb46ce13ee5a0486d5fa77ab2ad6974f2
dilawarafridi11 <dilawarafridi553@gmail.com>
2026-09-15 12:03:39 -0700

    chore: remove backend server from repository (keep only mobile app)

--- backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const dashboardController = require('../controllers/dashboardController');
-const { scopedShop } = require('../middleware/auth');
-const router = express.Router();
-
-router.get('/', scopedShop, dashboardController.getDashboard);
-
-module.exports = router;
